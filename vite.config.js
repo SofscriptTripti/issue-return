3f5cd9f2974489e1a04e92bdeee61d74dd0e0c85
SofscriptTripti <tripti.shukla@sofscript.com>
2026-02-19 12:45:26 +0530

    Updated PWA icons

--- vite.config.js
+++ vite.config.js
@@ -7,27 +7,31 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-    manifest: {
-  id: '/',
-  name: 'Issue Return',
-  short_name: 'IssueReturn',
-  start_url: '/',
-  display: 'standalone',
-  background_color: '#ffffff',
-  theme_color: '#000000',
-  icons: [
-    {
-      src: '/icon-192.png',
-      sizes: '192x192',
-      type: 'image/png'
-    },
-    {
-      src: '/icon-512.png',
-      sizes: '512x512',
-      type: 'image/png'
-    }
-  ]
+  manifest: {
+  name: "MediTrack - Medical Store",
+  short_name: "MediTrack",
+  description: "Medical Inventory Issue & Return System",
+  theme_color: "#0f172a",
+  background_color: "#0f172a",
+  display: "standalone",
+  orientation: "portrait",
+  start_url: "/",
+ "icons": [
+  {
+    "src": "/medical-192.jpg",
+    "sizes": "192x192",
+    "type": "image/jpeg"
+  },
+  {
+    "src": "/medical-512.jpg",
+    "sizes": "512x512",
+    "type": "image/jpeg"
+  }
+]
+
 }
+
+
 
     })
   ]
