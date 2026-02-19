dbf5ac6865844ba5f2b4fc75e5ad9fb870848d9a
SofscriptTripti <tripti.shukla@sofscript.com>
2026-02-19 11:26:03 +0530

    Initial commit - Issue Return PWA

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Issue Return</h1>
+      <p>Inventory Issue & Return Management System</p>
+    </div>
+  );
+}
+
+export default App;
